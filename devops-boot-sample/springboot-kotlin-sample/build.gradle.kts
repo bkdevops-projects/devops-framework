97b8e4bc72ac53703711d3516b97081336a5badc
carrypan <carrypan@tencent.com>
2020-09-17 18:01:48 +0800

    refactor: sample项目添加test依赖 #2

--- devops-boot-sample/springboot-kotlin-sample/build.gradle.kts
+++ devops-boot-sample/springboot-kotlin-sample/build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.tencent.devops:devops-boot-starter-demo")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }

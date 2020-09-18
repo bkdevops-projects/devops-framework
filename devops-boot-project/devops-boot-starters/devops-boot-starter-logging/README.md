d5376fcb3f72a1e3111318c37f6053cbad104b57
carrypan <carrypan@tencent.com>
2020-09-18 11:12:24 +0800

    feature: 初始化logging组件 #5

---
+++ devops-boot-project/devops-boot-starters/devops-boot-starter-logging/README.md
@@ -0,0 +1,7 @@
+# devops-boot-starter-logging
+
+## 功能介绍
+
+## 使用方式
+
+## 配置属性
---
+++ devops-boot-project/devops-boot-starters/devops-boot-starter-logging/build.gradle.kts
@@ -0,0 +1,5 @@
+description = "Starter for DevOps Boot Logging"
+
+dependencies {
+    api(project(":devops-boot-project:devops-boot-core:devops-logging"))
+}

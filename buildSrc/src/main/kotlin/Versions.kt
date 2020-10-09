669943a6c42cd38c79965267cf0803d55ca7c5d4
carrypan <carrypan@tencent.com>
2020-10-09 12:07:57 +0800

    dependencies: 升级springboot版本到2.3.4 #13

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Release {
 object Versions {
     const val Java = "1.8"
     const val Kotlin = "1.3.72"
-    const val SpringBoot = "2.3.3.RELEASE"
+    const val SpringBoot = "2.3.4.RELEASE"
     const val SpringCloud = "Hoxton.SR8"
     const val DependencyManagement = "1.0.10.RELEASE"
     const val NexusPublish = "0.4.0"

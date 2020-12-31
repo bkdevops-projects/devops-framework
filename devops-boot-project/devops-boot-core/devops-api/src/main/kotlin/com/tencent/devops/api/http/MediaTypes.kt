bb1b79fd8eb0d8a6fbb9f316ba2140c2e1813712
carrypan <carrypan@tencent.com>
2020-12-31 11:31:38 +0800

    feature: 提供HTTP常用枚举和常量 #43

---
+++ devops-boot-project/devops-boot-core/devops-api/src/main/kotlin/com/tencent/devops/api/http/MediaTypes.kt
@@ -0,0 +1,13 @@
+package com.tencent.devops.api.http
+
+/**
+ * HTTP Media Type常量
+ */
+object MediaTypes {
+    const val APPLICATION_OCTET_STREAM = "application/octet-stream"
+    const val APPLICATION_JSON = "application/json; charset=utf-8"
+    const val TEXT_HTML = "text/html"
+    const val APPLICATION_YAML = "application/x-yaml"
+    const val APPLICATION_TGZ = "application/x-tar"
+    const val APPLICATION_ICO= "image/x-icon"
+}

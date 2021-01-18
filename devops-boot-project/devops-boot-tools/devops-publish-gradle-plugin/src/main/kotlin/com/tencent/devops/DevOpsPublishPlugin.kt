d3add77f3203a81149ebddb6531ae0bbc12b68e8
carrypan <carrypan@tencent.com>
2021-01-18 15:16:26 +0800

    refactor: publish插件修改sonatype release仓库地址 #52

--- devops-boot-project/devops-boot-tools/devops-publish-gradle-plugin/src/main/kotlin/com/tencent/devops/DevOpsPublishPlugin.kt
+++ devops-boot-project/devops-boot-tools/devops-publish-gradle-plugin/src/main/kotlin/com/tencent/devops/DevOpsPublishPlugin.kt
@@ -157,7 +157,7 @@ class DevOpsPublishPlugin : Plugin<Project> {
         private const val SNAPSHOT_REPO_URL = "snapshotRepoUrl"
         private const val REPO_USERNAME = "repoUsername"
         private const val REPO_PASSWORD = "repoPassword"
-        private const val SONATYPE_RELEASE_REPO = "https://oss.sonatype.org/service/local/"
+        private const val SONATYPE_RELEASE_REPO = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
         private const val SONATYPE_SNAPSHOT_REPO = "https://oss.sonatype.org/content/repositories/snapshots/"
     }
 }

0c3d9dcc2b13ef51088dcd91f0236a8067822c28
carrypan <carrypan@tencent.com>
2021-03-09 21:02:04 +0800

    feature: copyToRelease支持不带版本号 #54

---
+++ devops-boot-project/devops-boot-tools/devops-boot-gradle-plugin/src/main/kotlin/com/tencent/devops/actions/KtLintFormatAction.kt
@@ -0,0 +1,31 @@
+package com.tencent.devops.actions
+
+import org.gradle.api.Action
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.JavaExec
+
+/**
+ * ktlint format action
+ */
+class KtLintFormatAction(
+    private val ktLint: FileCollection
+): Action<JavaExec> {
+
+    override fun execute(javaExec: JavaExec) {
+        with(javaExec) {
+            val outputDir = "${project.buildDir}/reports/ktlint/"
+            val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
+            group = "formatting"
+            inputs.files(inputFiles)
+            outputs.dir(outputDir)
+            description = "Fix Kotlin code style deviations."
+            classpath = ktLint
+            main = "com.pinterest.ktlint.Main"
+            args = listOf("-F", "src/**/*.kt")
+        }
+    }
+
+    companion object {
+        const val TASK_NAME = "ktlintFormat"
+    }
+}

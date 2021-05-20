91959ccb29ca12eeb86eac5bda685f657871724b
carrypan <carrypan@tencent.com>
2021-05-20 15:13:04 +0800

    feature: 提供客户端断路器&限流组件resilience4j #63

--- devops-boot-project/devops-boot-starters/devops-boot-starter-loadbalancer/build.gradle.kts
+++ devops-boot-project/devops-boot-starters/devops-boot-starter-loadbalancer/build.gradle.kts
@@ -1,4 +1,4 @@
-description = "Starter for DevOps Boot Loadbalancer"
+description = "Starter for DevOps Boot Load Balancer"
 
 dependencies {
     api(project(":devops-boot-project:devops-boot-core:devops-loadbalancer"))
--- devops-boot-sample/boot-kotlin-sample/src/main/kotlin/com/tencent/devops/sample/KotlinApplication.kt
+++ devops-boot-sample/boot-kotlin-sample/src/main/kotlin/com/tencent/devops/sample/KotlinApplication.kt
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 
+
 /**
  * 使用DevOpsBoot框架的Sample应用
  */

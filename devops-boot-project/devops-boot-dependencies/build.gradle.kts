bae9141936a21c8abdc1e19839cf2752109bcc5f
stubenhuang <stubenhuang@tencent.com>
2021-11-02 14:51:20 +0800

    bug: dependencies自定义的版本没有生效 bug: feign 10.10.1自动在header的value的逗号后面加空格, 导致获取到的值不符合预期

--- devops-boot-project/devops-boot-dependencies/build.gradle.kts
+++ devops-boot-project/devops-boot-dependencies/build.gradle.kts
@@ -19,26 +19,26 @@ dependencies {
     api(platform(MavenBom.SpringCloud))
 
     // jwt
-    api(enforcedPlatform("io.jsonwebtoken:jjwt-api:0.11.2"))
-    api(enforcedPlatform("io.jsonwebtoken:jjwt-impl:0.11.2"))
-    api(enforcedPlatform("io.jsonwebtoken:jjwt-jackson:0.11.2"))
+    api("io.jsonwebtoken:jjwt-api:0.11.2")
+    api("io.jsonwebtoken:jjwt-impl:0.11.2")
+    api("io.jsonwebtoken:jjwt-jackson:0.11.2")
 
     // apache common
-    api(enforcedPlatform("commons-io:commons-io:2.6"))
-    api(enforcedPlatform("org.apache.commons:commons-compress:1.18"))
-    api(enforcedPlatform("org.apache.hadoop:hadoop-hdfs:2.6.0"))
-    api(enforcedPlatform("org.apache.hadoop:hadoop-common:2.6.0"))
+    api("commons-io:commons-io:2.6")
+    api("org.apache.commons:commons-compress:1.18")
+    api("org.apache.hadoop:hadoop-hdfs:2.6.0")
+    api("org.apache.hadoop:hadoop-common:2.6.0")
 
     // guava
-    api(enforcedPlatform("com.google.guava:guava:29.0-jre"))
+    api("com.google.guava:guava:29.0-jre")
 
     // swagger
-    api(enforcedPlatform("io.swagger:swagger-annotations:1.5.20"))
-    api(enforcedPlatform("io.swagger:swagger-models:1.5.20"))
-    api(enforcedPlatform("io.springfox:springfox-boot-starter:3.0.0"))
+    api("io.swagger:swagger-annotations:1.5.20")
+    api("io.swagger:swagger-models:1.5.20")
+    api("io.springfox:springfox-boot-starter:3.0.0")
 
     // s3
-    api(enforcedPlatform("com.amazonaws:aws-java-sdk-s3:1.11.700"))
+    api("com.amazonaws:aws-java-sdk-s3:1.11.700")
 
     // TODO : it can be deleted after Spring cloud upgrade feign version (>= 11.4)
     api(enforcedPlatform("io.github.openfeign:feign-core:11.6"))

661f2025b6b4e2de4d58983f389c6be82488309d
royalhuang <royalhuang@tencent.com>
2022-04-11 12:31:36 +0800

    feature: 通过spring cloud stream方式支持pulsar bkdevops-projects#121

--- devops-boot-project/devops-boot-core/devops-pulsar/build.gradle.kts
+++ devops-boot-project/devops-boot-core/devops-pulsar/build.gradle.kts
@@ -1,9 +1,9 @@
 description = "DevOps Boot Pulsar"
 
 dependencies {
-    api("org.springframework.cloud:spring-cloud-stream:3.0.11.RELEASE")
+    api("org.springframework.cloud:spring-cloud-stream")
     api("org.springframework.boot:spring-boot-actuator")
     api("org.springframework.boot:spring-boot-actuator-autoconfigure")
-    api("org.apache.pulsar:pulsar-client:2.8.1")
-    api("com.google.protobuf:protobuf-java:3.19.4")
+    api("org.apache.pulsar:pulsar-client")
+    api("com.google.protobuf:protobuf-java")
 }

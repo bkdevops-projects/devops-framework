312873b5c86195d5761b584b4461c0880a4c7777
stubenhuang <stubenhuang@tencent.com>
2022-12-12 12:26:30 +0800

    feat: 默认关闭management.endpoint.devopsPlugin #155

--- devops-boot-project/devops-boot-core/devops-plugin/devops-plugin-core/src/main/kotlin/com/tencent/devops/plugin/spring/PluginEndpointAutoConfiguration.kt
+++ devops-boot-project/devops-boot-core/devops-plugin/devops-plugin-core/src/main/kotlin/com/tencent/devops/plugin/spring/PluginEndpointAutoConfiguration.kt
@@ -4,12 +4,14 @@ import com.tencent.devops.plugin.api.PluginManager
 import org.springframework.boot.actuate.autoconfigure.endpoint.EndpointAutoConfiguration
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(name = ["org.springframework.boot.actuate.endpoint.annotation.Endpoint"])
 @AutoConfigureAfter(EndpointAutoConfiguration::class)
+@ConditionalOnProperty(prefix = "management.endpoint.devopsPlugin", name = ["enabled"], havingValue = "false")
 class PluginEndpointAutoConfiguration {
 
     @Bean

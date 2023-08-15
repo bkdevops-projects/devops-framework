8a62951efe849535bc326c07a2b74b6e9ed0e340
felixncheng <felixncheng@tencent.com>
2023-08-15 11:49:13 +0800

    feat: swagger支持basic auth #180

--- devops-boot-project/devops-boot-core/devops-web/src/main/kotlin/com/tencent/devops/web/swagger/SwaggerConfiguration.kt
+++ devops-boot-project/devops-boot-core/devops-web/src/main/kotlin/com/tencent/devops/web/swagger/SwaggerConfiguration.kt
@@ -9,7 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.HttpAuthenticationScheme
+import springfox.documentation.service.SecurityReference
+import springfox.documentation.service.SecurityScheme
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider
 import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider
@@ -43,16 +47,39 @@ class SwaggerConfiguration {
             .apis(RequestHandlerSelectors.withClassAnnotation(RestController::class.java))
             .paths(PathSelectors.any())
             .build()
+            .securityContexts(securityContexts())
+            .securitySchemes(securitySchemes())
+    }
+
+    private fun securitySchemes(): List<SecurityScheme> {
+        val basicAuthScheme = HttpAuthenticationScheme.BASIC_AUTH_BUILDER
+            .name(BASIC_AUTH)
+            .description("Basic authorization")
+            .build()
+        return listOf(basicAuthScheme)
+    }
+
+    private fun securityContexts(): List<SecurityContext> {
+        val securityContext = SecurityContext.builder()
+            .securityReferences(basicAuthReference())
+            .operationSelector { true }
+            .build()
+        return listOf(securityContext)
+    }
+
+    private fun basicAuthReference(): List<SecurityReference> {
+        val basicSecurityReference = SecurityReference(BASIC_AUTH, emptyArray())
+        return listOf(basicSecurityReference)
     }
 
     /**
-     * 处理升级swagger启动报错
+     * 处理升级springboot,导致的swagger启动报错
      * @see <a href="https://github.com/springfox/springfox/issues/3462">bug in springfox</a>
      * */
     @Bean
     fun springfoxHandlerProviderBeanPostProcessor(): BeanPostProcessor {
         return object : BeanPostProcessor {
-            override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
+            override fun postProcessAfterInitialization(bean: Any, beanName: String): Any {
                 if (bean is WebMvcRequestHandlerProvider || bean is WebFluxRequestHandlerProvider) {
                     customizeSpringfoxHandlerMappings(getHandlerMappings(bean))
                 }
@@ -70,11 +97,16 @@ class SwaggerConfiguration {
                 mappings.addAll(copy)
             }
 
+            @Suppress("UNCHECKED_CAST")
             private fun getHandlerMappings(bean: Any): MutableList<Any> {
                 val field = ReflectionUtils.findField(bean.javaClass, "handlerMappings") ?: return mutableListOf()
                 field.isAccessible = true
                 return field.get(bean) as MutableList<Any>
             }
         }
+    }
+
+    companion object {
+        private const val BASIC_AUTH = "basicAuth"
     }
 }

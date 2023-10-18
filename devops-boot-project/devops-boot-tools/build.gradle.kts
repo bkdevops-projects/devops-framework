e5e8b0d55e6f45371c63f2c8a5abb1491fac33b8
stubenhuang <stubenhuang@tencent.com>
2023-10-18 16:29:10 +0800

    feat: 升级jdk到17 #197

--- devops-boot-project/devops-boot-tools/build.gradle.kts
+++ devops-boot-project/devops-boot-tools/build.gradle.kts
@@ -1,7 +1,14 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 description = "Tools for DevOps Boot"
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "publish")
+
+    tasks {
+        getByName<KotlinCompile>("compileKotlin") {
+            kotlinOptions.jvmTarget = Versions.Java
+        }
+    }
 }

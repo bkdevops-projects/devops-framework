e2f8a1a8ee187ef532996cd38ea3e0c9aaa5e4a7
felixncheng <felixncheng@tencent.com>
2024-07-02 16:18:27 +0800

    feat: Plugin支持第三方jar #201

--- devops-boot-project/devops-boot-core/devops-plugin/devops-plugin-core/build.gradle.kts
+++ devops-boot-project/devops-boot-core/devops-plugin/devops-plugin-core/build.gradle.kts
@@ -4,5 +4,6 @@ dependencies {
     api(project(":devops-boot-project:devops-boot-core:devops-plugin:devops-plugin-api"))
     api("org.springframework.boot:spring-boot-starter")
     api("org.springframework:spring-webmvc")
+    api("org.springframework.boot:spring-boot-loader")
     compileOnly("org.springframework.boot:spring-boot-starter-actuator")
 }

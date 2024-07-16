f9782a1dd014589416b6ad70fdba1d994c26d50f
felixncheng <felixncheng@tencent.com>
2024-07-16 19:37:44 +0800

    feat: 修复FeignClient不支持RequestMapping #205

--- devops-boot-project/devops-boot-core/devops-service-servlet/src/main/kotlin/com/tencent/devops/service/ServiceServletAutoConfiguration.kt
+++ devops-boot-project/devops-boot-core/devops-service-servlet/src/main/kotlin/com/tencent/devops/service/ServiceServletAutoConfiguration.kt
@@ -1,13 +1,18 @@
 package com.tencent.devops.service
 
+import com.tencent.devops.service.feign.CustomSpringMvcContract
 import com.tencent.devops.service.feign.FeignFilterRequestMappingHandlerMapping
+import feign.Contract
 import org.springframework.boot.autoconfigure.AutoConfigureBefore
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations
+import org.springframework.cloud.openfeign.AnnotatedParameterProcessor
+import org.springframework.cloud.openfeign.FeignClientProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
+import org.springframework.core.convert.ConversionService
 
 /**
  * Service自动化配置类
@@ -23,5 +28,15 @@ class ServiceServletAutoConfiguration {
         return object : WebMvcRegistrations {
             override fun getRequestMappingHandlerMapping() = FeignFilterRequestMappingHandlerMapping()
         }
+    }
+
+    @Bean
+    fun feignContract(
+        feignClientProperties: FeignClientProperties?,
+        parameterProcessors: List<AnnotatedParameterProcessor>,
+        feignConversionService: ConversionService,
+    ): Contract {
+        val decodeSlash = feignClientProperties == null || feignClientProperties.isDecodeSlash
+        return CustomSpringMvcContract(parameterProcessors, feignConversionService, decodeSlash)
     }
 }

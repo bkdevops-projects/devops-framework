1f2b5eadc9c0f1822e1dda27ebf828a16317f524
stubenhuang <stubenhuang@tencent.com>
2025-03-19 16:57:47 +0800

    spring.factories修改

--- devops-boot-project/devops-boot-core/devops-logging/src/main/kotlin/com/tencent/devops/logging/LoggingAutoConfiguration.kt
+++ devops-boot-project/devops-boot-core/devops-logging/src/main/kotlin/com/tencent/devops/logging/LoggingAutoConfiguration.kt
@@ -1,11 +1,16 @@
 package com.tencent.devops.logging
 
 import com.tencent.devops.logging.props.DevopsLoggingProperties
+import com.tencent.devops.logging.system.DevopsLoggingApplicationListener
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnWebApplication
 @EnableConfigurationProperties(DevopsLoggingProperties::class)
-class LoggingAutoConfiguration
+class LoggingAutoConfiguration {
+    @Bean
+    fun devopsLoggingApplicationListener() = DevopsLoggingApplicationListener()
+}
